9855bb4259f7f6a01b8cf69007403ab3fb07423f
usuario <David>
2022-10-02 23:31:14 +0000

    me queda lo final

--- P1_simbolos_alfabetos_cadenas/funciones_cadenas.h
+++ P1_simbolos_alfabetos_cadenas/funciones_cadenas.h
@@ -6,44 +6,33 @@
 // Autor: Francisco David Hernández Alayón
 // Correo: alu0101469898@ull.edu.es
 // Fecha: 04/10/2022
-// Archivo funciones_cadenas.cc: Fichero que contiene las funciones del main
+// Archivo funciones_cadenas.h: Fichero que contiene las definiciones de las funciones del main
 // Historial de revisiones
 // 02/09/2022 - Creación, primera versión del código
 
 
 #include <iostream>
-#include <fstream>
 
-#include "clase_alfabeto.h"
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
 
 // Método para sacar la cadena y el alfabeto de cada linea
-void ObtieneAlfabetoCadena(const std::string& kNombreFicheroEntrada, Alfabeto& alfabeto, Cadena cadena) {
-
-
-}
+void ObtieneAlfabetoCadena(const std::string& kNombreFicheroEntrada, Alfabeto& alfabeto, Cadena cadena);
 
 // Método para saber la longitud de una cadena
-void Longitud(const std::ofstream& kFicheroSalida) {
-
-}
+void Longitud(const std::ofstream& kFicheroSalida);
 
 // Método para saber linvertir una cadena
-void Invertir(const std::ofstream& kFicheroSalida) {
-
-}
+void Invertir(const std::ofstream& kFicheroSalida);
 
 // Método para saber los prefijos de una cadena
-void Prefijos(const std::ofstream& kFicheroSalida) {
-
-}
+void Prefijos(const std::ofstream& kFicheroSalida);
 
 // Método para saber los sufijos de una cadena
-void Sufijos(const std::ofstream& kFicheroSalida) {
-
-}
+void Sufijos(const std::ofstream& kFicheroSalida);
 
 // Método para saber las subcadenas de una cadena
-void Subcadenas(const std::ofstream& kFicheroSalida) {
+void Subcadenas(const std::ofstream& kFicheroSalida);
 
-}
-
+#endif
--- P1_simbolos_alfabetos_cadenas/main_cadenas.cc
+++ P1_simbolos_alfabetos_cadenas/main_cadenas.cc
@@ -14,7 +14,7 @@
 #include <iostream>
 
 #include "clase_alfabeto.h"
-#include "funciones_cadenas.cc"
+#include "funciones_cadenas.h"
 
 // Método que sirve para mostrar al usuario la forma de interactuar con el programa
 void Usage(int argc, char *argv[]) {
@@ -37,30 +37,46 @@ void Usage(int argc, char *argv[]) {
 }
 
 
+
 int main(int argc, char* argv[]) {
   Usage(argc, argv);
   std::string parametro{argv[3]};  // opcion escogida por el usuario
+  std::string fichero_entrada{argv[1]};  // fichero de entrada
+  std::string fichero_salida{argv[2]};  // fichero de salida
+
 
   if (parametro.length() == 1) { // comprobamos is el opcode es un solo caracter
     int opcion_usuario{std::stoi(parametro)};
-    switch(opcion_usuario) {    // comprobamos que opcion a elegido el usuario
-      case 1:
-      break;
+    std::ifstream fichero_leer{fichero_entrada};
+    std::ofstream fichero_escribir{fichero_salida};
+    std::string linea_actual;
 
-      case 2:
-      break;
+    while (getline(fichero_leer, linea_actual)) {
+      Simbolo simbolo_vacio{"&"};
+      Alfabeto alfabeto{simbolo_vacio};  // alfabeto para rellenar
+      Cadena cadena;  // cadena para rellenar
+      ObtieneAlfabetoCadena(linea_actual, alfabeto, cadena);  // metodo para sacar la cadena y el alfabeto apartir el fichero de entrada
+      
+      switch(opcion_usuario) {    // comprobamos que opcion a elegido el usuario
+        case 1: Longitud(fichero_escribir);
+        break;
 
-      case 3:
-      break;
+        case 2: Invertir(fichero_escribir);
+        break;
 
-      case 4:
-      break;
+        case 3: Prefijos(fichero_escribir);
+        break;
 
-      case 5:
-      break;
+        case 4: Sufijos(fichero_escribir);
+        break;
 
-      default: std::cout << "El opcode introducido no es válido, introduzca uno entre el 1 y el 5" << std::endl;
-      break;
+        case 5: Subcadenas(fichero_escribir);
+        break;
+
+        default: std::cout << "El opcode introducido no es válido, introduzca uno entre el 1 y el 5" << std::endl;
+        break;
+      }
+    
     }
   } else {
     std::cout << "El opcode introducido no es válido, introduzca uno entre el 1 y el 5 " << std::endl;
